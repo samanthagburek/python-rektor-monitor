9a87d2097a37deef0c0c54073393aa60efab41f5
samanthagburek <sgg2056@nyu.edu>
2024-11-10 16:23:59 -0500

    tests and poetry fixed

---
+++ tests/test_consistency_no_tree_id.py
@@ -0,0 +1,21 @@
+import sys
+import json
+from jsonschema import validate
+import subprocess
+import requests
+
+def test_consistency():
+    no_tree_output = "please specify tree size for prev checkpoint\n"
+
+    consistency = subprocess.run(
+        ['python3', 'main.py', '--consistency', '--tree-id', '344567'],
+        capture_output=True,
+        text=True
+    )
+    consistency_output = consistency.stdout
+
+    assert no_tree_output == consistency_output
+
+
+if __name__ == "__main__":
+    test_consistency()
---
+++ tests/test_debug.py
@@ -0,0 +1,18 @@
+import sys
+import json
+from jsonschema import validate
+import subprocess
+
+def test_debug():
+    debugmode = "enabled debug mode\n"
+    result = subprocess.run(
+        ['python3', 'main.py', "--debug"],
+        capture_output=True,
+        text=True
+    )
+    output = result.stdout
+
+    assert debugmode == output
+
+if __name__ == "__main__":
+    test_debug()
